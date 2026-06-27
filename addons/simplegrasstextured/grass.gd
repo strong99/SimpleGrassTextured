a3127131f32de46cfd243a9a39e9325d55fd5249
KnightRex <rex@4vandorp.nl>
2026-06-27 09:16:14 +0200

    Fixed transformation issues for the pencil tool and while running the game world.

--- addons/simplegrasstextured/grass.gd
+++ addons/simplegrasstextured/grass.gd
@@ -637,8 +637,8 @@ func _update_height_map() -> void:
 		0,
 		(aabb.position.z + (aabb.size.z / 2.0))
 	)
-	_node_height_map.global_position = global_position + align
-	_node_height_map.visible = visible
+	_node_height_map.global_position = global_position + (global_basis * align)
+	_node_height_map.global_rotation = global_rotation
 
 
 func _update_material_shader() -> bool:
